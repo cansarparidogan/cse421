34d32098641edb7e4cef2567251537da8d5ac95e
cansarparidogan <cansarp.aridogan@std.yeditepe.edu.tr>
2025-12-14 14:41:04 +0300

    Delete hw3/q1_temp/q2_har directory

--- hw3/q1_temp/q2_har/mbed/q2_har_model.h
+++
@@ -1,7 +0,0 @@
-#ifndef Q2_HAR_MODEL_H
-#define Q2_HAR_MODEL_H
-#define Q2_NUM_FEATURES 10
-float q2_sigmoid(float x);
-float q2_predict_prob(const float x[Q2_NUM_FEATURES]);
-int q2_predict_label(const float x[Q2_NUM_FEATURES]);
-#endif
